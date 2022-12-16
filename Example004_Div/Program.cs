07bc038624ca52e79ff690eb3818f98076c45a88
Stexov <stexovdim@gmail.com>
2022-12-16 11:42:44 +0100

    Заменил файл gitignore

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,4 @@
-﻿double numberA = 10;
-double numberB = 3;
-double result = numberA / numberB;
+﻿double numberA = 456;
+double numberB = 10;
+double result = numberA % numberB;
 Console.WriteLine(result);
